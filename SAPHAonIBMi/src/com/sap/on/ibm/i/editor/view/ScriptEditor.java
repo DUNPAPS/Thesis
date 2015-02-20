3a783cff352537a0a05ef6e5620f45d6bfd7f785
DUNPAPS <dunpaps@yahoo.com>
2015-02-20 17:33:43 +0100

    commits
    Change-Id: I25e2864d00c695cb92455390d09c1ea4b2cfdc1b
    Signed-off-by: DUNPAPS <dunpaps@yahoo.com>

--- SAPHAonIBMi/src/com/sap/on/ibm/i/editor/view/ScriptEditor.java
+++ SAPHAonIBMi/src/com/sap/on/ibm/i/editor/view/ScriptEditor.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.BadLocationException;
 
 import com.sap.on.ibm.i.editor.controller.Controller;
 
@@ -14,10 +15,10 @@ import java.io.*;
 public class ScriptEditor extends JFrame {
 
 	private JFileChooser fileChooser;
-	private JTextArea output = new JTextArea(10, 40);
-	private final JButton btnRunScript = new JButton("Run Script");
+	private JTextArea textarea = new JTextArea(10, 40);
 	private Controller controller;
 	private File file;
+	private boolean waitCursorIsShowing;
 
 	public ScriptEditor(Controller controller) {
 		this.controller = controller;
@@ -46,7 +47,7 @@ public class ScriptEditor extends JFrame {
 					try {
 						file = fileChooser.getSelectedFile();
 						FileReader fr = new FileReader(file);
-						output.read(fr, file);
+						textarea.read(fr, file);
 						fr.close();
 					} catch (Exception ex) {
 						ex.printStackTrace();
@@ -62,67 +63,180 @@ public class ScriptEditor extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				saveAs();
-//				JOptionPane.showMessageDialog(null,
-//						"The Message was Saved Successfully! ");
+				// JOptionPane.showMessageDialog(null,
+				// "The Message was Saved Successfully! ");
 			}
+		};
+
+		Action runScript = new AbstractAction("Run Script") {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (textarea.getText().equals("")) {
+					Cursor defaultCursor = new Cursor(Cursor.DEFAULT_CURSOR);
+					setCursor(defaultCursor);
+
+					JOptionPane.showMessageDialog(new JFrame(),
+							"There is no script to run", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+
+					Cursor waitCursor = new Cursor(Cursor.WAIT_CURSOR);
+					try {
+						synchronized (waitCursor) {
+							setCursor(Cursor
+									.getPredefinedCursor(Cursor.WAIT_CURSOR));
+							waitCursor.wait(900);
+						}
+
+						int totalLines = textarea.getLineCount();
+						String text = textarea.getText();
+						for (int i = 0; i < totalLines; i++) {
+							int start = textarea.getLineStartOffset(i);
+							int end = textarea.getLineEndOffset(i);
+							String line = text.substring(start, end);
+							if (!line.equals("") && line.contains(":")) {
+								String[] split = line.split(":");
+								String firstSubString = split[0];
+								String secondSubString = split[1];
+
+								if (firstSubString.trim().equals("Step0")
+										&& secondSubString.trim().equals(
+												"StopSAP")) {
+									controller.get_outputTestEditor()
+											.getStop_SAP_Checkbox()
+											.setSelected(true);
+								}
+								if (firstSubString.trim().equals("Step1")
+										&& secondSubString.trim().equals(
+												"ApplyKernel")) {
+									controller.get_outputTestEditor()
+											.getApplyKernelCheckbox()
+											.setSelected(true);
+								}
+								if (firstSubString.trim().equals("Step2")
+										&& secondSubString.trim().equals(
+												"StartSAP")) {
+									controller.get_outputTestEditor()
+											.getStartSAPCheckBox()
+											.setSelected(true);
+								}
+								if (firstSubString.trim().equals("SAP_SID")
+										&& secondSubString.trim().equals("DCN")) {
+									controller.get_outputTestEditor()
+											.getSid_field().setText("");
+									controller.get_outputTestEditor()
+											.getSid_field().setText("dcnadm");
+									System.out.println(controller.get_outputTestEditor()
+											.getSid_field().getText());
+								}
+								if (firstSubString.trim().equals("SAP_User")
+										&& secondSubString.trim().equals(
+												"dcnadm")) {
+									controller.get_outputTestEditor()
+											.getPassword_field().setText("");
+									controller.get_outputTestEditor()
+											.getPassword_field()
+											.setText("sapofr12");
+									System.out.println(controller.get_outputTestEditor()
+											.getPassword_field().getText());
+								}
+								if (firstSubString.trim()
+										.equals("SAP_Password")
+										&& secondSubString.trim().equals(
+												"sapofr12")) {
+									// TODO
+								}
+
+							}
+						}
+
+					} catch (InterruptedException | BadLocationException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					setCursor(waitCursor);
+
+					Cursor defaultCursor = new Cursor(Cursor.DEFAULT_CURSOR);
+					setCursor(defaultCursor);
+					setInVisible();
+				}
+
+			}
+
 		};
 
 		JToolBar tb = new JToolBar();
 		gui.add(tb, BorderLayout.PAGE_START);
-		JButton button_1 = tb.add(open);
-		button_1.setFont(new Font("Arial", Font.PLAIN, 12));
-		JButton button = tb.add(save);
-		button.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnRunScript.setFont(new Font("Arial", Font.PLAIN, 12));
+		JButton openButton = tb.add(open);
+		openButton.setFont(new Font("Arial", Font.PLAIN, 12));
+		JButton saveButton = tb.add(save);
+		saveButton.setFont(new Font("Arial", Font.PLAIN, 12));
+		JButton runScriptButton = tb.add(runScript);
+		runScriptButton.setFont(new Font("Arial", Font.PLAIN, 12));
 
-		tb.add(btnRunScript);
+		textarea.setWrapStyleWord(true);
+		textarea.setLineWrap(true);
 
-		output.setWrapStyleWord(true);
-		output.setLineWrap(true);
-
-		gui.add(new JScrollPane(output, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+		gui.add(new JScrollPane(textarea,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
 		return gui;
 	}
 
 	public void saveAs() {
-	      FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("Text File", "txt");
-	      final JFileChooser saveAsFileChooser = new JFileChooser();
-	      saveAsFileChooser.setApproveButtonText("Save");
-	      saveAsFileChooser.setFileFilter(extensionFilter);
-	      int actionDialog = saveAsFileChooser.showOpenDialog(this);
-	      if (actionDialog != JFileChooser.APPROVE_OPTION) {
-	         return;
-	      }
-	   // !! File fileName = new File(SaveAs.getSelectedFile() + ".txt");
-	      File file = saveAsFileChooser.getSelectedFile();
-	      if (!file.getName().endsWith(".txt")) {
-	         file = new File(file.getAbsolutePath() + ".txt");
-	      }
+		FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter(
+				"Text File", "txt");
+		final JFileChooser saveAsFileChooser = new JFileChooser();
+		saveAsFileChooser.setApproveButtonText("Save");
+		saveAsFileChooser.setFileFilter(extensionFilter);
+		int actionDialog = saveAsFileChooser.showOpenDialog(this);
+		if (actionDialog != JFileChooser.APPROVE_OPTION) {
+			return;
+		}
+		// !! File fileName = new File(SaveAs.getSelectedFile() + ".txt");
+		File file = saveAsFileChooser.getSelectedFile();
+		if (!file.getName().endsWith(".txt")) {
+			file = new File(file.getAbsolutePath() + ".txt");
+		}
 
-	      BufferedWriter outFile = null;
-	      try {
-	         outFile = new BufferedWriter(new FileWriter(file));
+		BufferedWriter outFile = null;
+		try {
+			outFile = new BufferedWriter(new FileWriter(file));
 
-	         output.write(outFile);
+			textarea.write(outFile);
 
-	      } catch (IOException ex) {
-	         ex.printStackTrace();
-	      } finally {
-	         if (outFile != null) {
-	            try {
-	               outFile.close();
-	            } catch (IOException e) {}
-	         }
-	      }
-	   }
- 
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (outFile != null) {
+				try {
+					outFile.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+
 	public void setVisible() {
 		Runnable r = new Runnable() {
 
 			@Override
 			public void run() {
 				setVisible(true);
+				setLocationRelativeTo(null);
+
+			}
+		};
+		SwingUtilities.invokeLater(r);
+	}
+
+	public void setInVisible() {
+		Runnable r = new Runnable() {
+
+			@Override
+			public void run() {
+				setVisible(false);
 				setLocationRelativeTo(null);
 
 			}
